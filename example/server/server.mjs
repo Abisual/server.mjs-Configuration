6e3ac87215501c40a61742ebf97ecc787c8f50f8
Andrey Utkin <45770262+Abisual@users.noreply.github.com>
2020-11-22 01:34:45 +0300

    chat added

--- example/server/server.mjs
+++ example/server/server.mjs
@@ -3,6 +3,9 @@
     Updated 22.11 by BRILLZ
 */
 import * as alt from 'alt';
+import chat from 'chat';
+
+console.log(chat);
 
 const spawnPos = {
     x: -2639.872,
@@ -12,6 +15,28 @@ const spawnPos = {
 
 alt.on('playerConnect', (player) => {
     player.model = 'mp_m_freemode_01';
-    player.spawn(spawnPos.x, spawnPos.y, spawnPos.z, 5000);
-
+    player.spawn(spawnPos.x, spawnPos.y, spawnPos.z, 0);
 });
+
+chat.registerCmd('sethp', (player, arg) => {
+    if (!arg || arg.lenght <=0){
+        chat.send(player, '/sethp (amount)')
+        return;
+    }
+
+    let amount = parseInt(arg[0]);
+    if (amount < 100) {
+        amount += 100;
+    }
+
+    if (isNaN(amount)){
+        chat.send(player, 'The amount specified; was not a number.');
+        return;
+    }
+    player.health = amount;
+})
+
+chat.registerCmd('Hello',(player, arg) => {
+    chat.send(player, 'world')
+});
+
